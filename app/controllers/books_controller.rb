303eb6ae74a5ae76b34b34c53e9a602ff2566709
EC2 Default User <ec2-user@ip-172-31-17-18.ec2.internal>
2022-03-26 12:24:47 +0000

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
-    else
-      render :new
-    end
-    if @book.save
       flash[:alert] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
     end
   end
 
@@ -22,6 +21,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def destroy
@@ -36,14 +36,16 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if list.update(book_params)
-    flash[:alert] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+       flash[:alert] = "Book was successfully updated."
+    else
+       render :edit
     end
   end
 
-   private
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
